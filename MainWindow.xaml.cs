24cdbef8af5cffe3609338a7ef053032b6c0d7a1
safonov <safonov@is35.ru>
2022-12-08 12:09:34 +0300

    Первая заливка.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace ConcatPdf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            MainWindow window = new MainWindow();
+            window.ShowDialog();
+            return 0;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
